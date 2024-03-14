fd497333365add06e0219f321ffdeed33e99b70f
Martin SEBIROT <martinzs1@yahoo.fr>
2024-03-14 11:24:42 +0100

    users endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const app = express();
 const port = 3000;
@@ -10,3 +12,26 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+// USERS
+
+app.get('/api/users/:id', async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    });
+    res.json(user);
+});
+
+app.put('/api/users', async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    });
+    res.json(user);
+});
+
+

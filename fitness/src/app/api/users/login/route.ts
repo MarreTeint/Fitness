9115f731d69f40d545232b6450feba3949ea9b8b
kevin_LEBORGNE <leborgnekevin430@gmail.com>
2024-04-23 14:45:20 +0200

    user clean api

--- fitness/src/app/api/users/login/route.ts
+++ fitness/src/app/api/users/login/route.ts
@@ -1,24 +1,9 @@
 import {NextRequest, NextResponse} from "next/server";
-import { logUser,LogUserError } from "@/prisma/userManager";
+import { logUserUseCase } from "@/usecase/UserUseCase";
 
 //log user with email and password
 export async function POST (request: NextRequest){
-    if (!request.body) {
-        return NextResponse.json({ error: 'Request body is empty' },{status: 400});
-    }
-    const body = await request.json();
-    const { email, password} = body;
-
-    try {
-        const user = await logUser(email, password);
-        return NextResponse.json(user, {status: 200});
-    } catch (error) {
-        if (error instanceof LogUserError) {
-            return NextResponse.json({ error: error.message }, {status: 400});            
-        }
-        return NextResponse.json({error}, {status: 400});
-    }
-
+    return await logUserUseCase(request);
 }
 
     
